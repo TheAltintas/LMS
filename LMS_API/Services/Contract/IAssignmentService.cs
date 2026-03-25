dd52b2ace73ad9fc6fb17b1d8218f0595fd6b2de
shoaibalirts <shoaibali.rts@gmail.com>
2026-03-25 14:11:51 +0100

    Create IAssignmentService interface that holds 3 signatures

---
+++ LMS_API/Services/Contract/IAssignmentService.cs
@@ -0,0 +1,12 @@
+﻿using LMS_API.Models;
+using LMS_API.Models.DTO.Assignment;
+
+namespace LMS_API.Services.Contract
+{
+    public interface IAssignmentService
+    {
+        Task<Assignment> CreateAssignmentAsync(AssignmentCreateDTO assignmentDTO);
+        Task<IEnumerable<Assignment>> GetAllAssignmentsAsync();
+        Task<bool> DeleteAssignmentAsync(int id);
+    }
+}
